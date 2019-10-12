45bfa98e2ede168ea2180fd951c3101f6306e2e9
Edoardo de Cal <edoardo.decal@gmail.com>
2019-10-12 10:46:54 +0200

    added some files

---
+++ MLHelpDevices/MLHelpDevices/ViewController.swift
@@ -0,0 +1,16 @@
+//  Created by Edoardo de Cal on 10/12/19.
+//  Copyright © 2019 Edoardo de Cal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
